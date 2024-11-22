f98905618c92ece0f7e4c978daa33e74dc5f5c22
carson-leigh <carson.leigh@outlook.com>
2024-11-22 15:32:02 -0600

    fix: RegisterActivity not using Singleton feat[ui]: Added goal type to GoalFragment add button

--- app/src/main/java/com/cse3310/myfitnesstracker/ui/goals/GoalsFragment.java
+++ app/src/main/java/com/cse3310/myfitnesstracker/ui/goals/GoalsFragment.java
@@ -17,15 +17,6 @@ import android.widget.LinearLayout;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
-import android.app.AlertDialog;
-import android.os.Bundle;
-import android.text.TextUtils;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
 import com.cse3310.myfitnesstracker.FitnessDatabaseHelper;
 import com.cse3310.myfitnesstracker.R;
